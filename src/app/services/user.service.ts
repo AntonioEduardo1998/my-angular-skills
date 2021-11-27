4fd2c6b04d4ef9e8c99f4ff708aa082407e28456
Antonio Eduardo <antonioeduardo1998@gmail.com>
2021-11-27 16:14:37 -0300

    feat: update api path

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { pluck } from 'rxjs/operators';
 
 import User from '@src/app/models/User';
 import { environment } from '@src/environments/environment';
@@ -13,8 +12,6 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   public getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${environment.baseUrl}/db`).pipe(
-      pluck('users'),
-    );
+    return this.http.get<User[]>(`${environment.baseUrl}/users`);
   }
 }
